61b1ef64b5aeaa936d33e87c8d0323cef02803a5
ben <benjamin.n.chapin@gmail.com>
2020-02-27 10:37:14 -0600

    first commit

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="products-index">
+    <h1>{{ message }}</h1>
+    <br />
+    <h1>All Products</h1>
+    <div v-for="product in products">
+      <h2>{{ product.name }}</h2>
+      <router-link :to="`/products/${product.id}`">More Info</router-link>
+      <br />
+      <img src="${product.images.url}" />
+      <p>Description: {{ product.description }}</p>
+      <p>Height: {{ product.price }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      message: "Welcome to The Smart People Store!",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(response => {
+      this.products = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
